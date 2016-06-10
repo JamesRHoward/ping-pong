9073b2bea446a9ef6387d6d5e215fa0e11719fa9
James Howard <james@>
2016-06-10 09:17:46 -0700

    created business logic. pull old jquery file from github, bootsrap wont work with jquery-3.0.0.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
-// UI logic
+// Business Logic
+var pingpong = function(pong) {
+  if (pong % 3 === 0) {
+    return ping;
+  }
+  if (pong % 5 === 0) {
+    return pong;
+  }
+  if (pong % 15 === 0) {
+    return pingpong
+  }else {
+    return "#game";
+  }
+};
+// UI Logic
 $(document).ready(function(){
   $("#pingpong").submit(function(event){
     event.preventDefault();
